50894c8977530db8b984a47a1b9fb58a5f9a2bb9
Stormlight-Coding <jonposo.music@gmail.com>
2020-05-06 13:04:30 -0700

    second with addition of features

--- state-app/src/App.js
+++ state-app/src/App.js
@@ -5,7 +5,9 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      count: 0
+      count: 0,
+      originalColor: 'none',
+      newColor: 'fifteen'
     }
   }
 
@@ -13,11 +15,17 @@ class App extends React.Component {
     this.setState(prevState => ({
         count: prevState.count + 1
     }));
+    if (this.state.count > 13){
+      this.setState(prevState => ({
+        originalColor: 'fifteen'
+      }));
+    }
   }
   render() {
     return (
-      <div>
+      <div className={this.state.originalColor}>
           <h1>{this.state.count}</h1>
+          <p>{this.state.originalColor}</p>
           <button
               onClick={()=> this.updateCount()} > +
           </button>
